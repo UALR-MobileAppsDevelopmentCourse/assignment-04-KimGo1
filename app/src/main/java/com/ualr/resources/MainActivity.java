f726b56b5e278ac655a5b50a9e39f2170513a758
KimGo1 <Kimberlygoods36@gmail.com>
2023-10-09 20:35:10 -0500

    more changes

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -1,9 +1,18 @@
 package com.ualr.resources;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.provider.Settings.System.getConfiguration;
 
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Locale;
+
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button; //need to import Button class
+import android.widget.ImageView;
+import android.widget.TextView; // Import TextView class
+
+import com.ualr.resources.databinding.ActivityMainBinding;
 
 // TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
 // TODO 4: Get the description string value from resources
@@ -11,17 +20,59 @@ import android.widget.Button; //need to import Button class
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding activityMainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = activityMainBinding.getRoot();
+        setContentView(view);
 
-        //Find the lesson_learnt_button by its ID
-        Button lessonLearntButton = findViewById(R.id.lesson_learnt_button);
+        // Set a new source image for the ImageView
+
+        activityMainBinding.svgImageView.setImageResource(R.drawable.spain);
+
+        // Set a new text for the TextView with the new task description
+        int setTaskDescription = R.string.country_description_es;
+        activityMainBinding.countryDescriptionText.setText(setTaskDescription);
+
+        // Get the actual color value
+        Resources res = this.getResources();
+        activityMainBinding.countryDescriptionText.setTextColor(res.getColor(R.color.colorAccent));
+
+
+        activityMainBinding.usaImageView.setImageResource(R.drawable.usa_bitmap);
+
+        // Set an OnClickListener on the Button
+        activityMainBinding.lessonLearntButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // Get the current locale of the device
+        Locale currentLocale = getResources().getConfiguration().locale;
+
+        // Define the Spanish locale
+        Locale spanishLocale = new Locale("es"); // "es" represents the Spanish language
+
+
+        String description = getString(R.string.country_description_default); //Default language
+        if (currentLocale.equals(spanishLocale)) {
+            description = getString(R.string.country_description_es); // Spanish language
+        } else {
+            // Default to another English language
+        }
+
+        activityMainBinding.countryDescriptionText.setText(description);
 
         // Set the text for the lessonLearntButton using the string resource
-        lessonLearntButton.setText(getString(R.string.lesson_learnt_button_text));
-    }
+        activityMainBinding.lessonLearntButton.setText(getString(R.string.lesson_learnt_button_text));
+
+            }
 }
 
 
--- app/src/main/java/com/ualr/resources/Utils.java
+++ app/src/main/java/com/ualr/resources/Utils.java
@@ -1,2 +1,9 @@
-package com.ualr.resources;public class Utils {
+package com.ualr.resources;
+
+import android.content.Context;
+
+public class Utils {
+    public static int getColorValue(Context context, int colorResourceId) {
+        return context.getApplicationContext().getResources().getColor(colorResourceId);
+    }
 }
